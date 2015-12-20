c03921bc3ce51454c07fa0c06d623a712bcecf38
Chris Neal <me@chrisrneal.com>
2015-12-20 00:07:21 +0000

    adds user model, and user display on all posts page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :description, presence: true
+    validates :user_id, presence: true
+    belongs_to :user
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+    has_many :posts
+    before_save { 
+        self.email = email.downcase
+        self.username = username.downcase
+    }
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum:30}
+    validates :email, presence: true, uniqueness: {case_sensitive: false} 
+    validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+end
