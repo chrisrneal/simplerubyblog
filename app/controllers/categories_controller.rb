e924fdc29b03145674277abf19a8804045afcf31
Chris Neal <me@chrisrneal.com>
2015-12-27 16:09:26 +0000

    enables category editting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
     
     before_action :require_admin_user, except: [:index, :show]
+    before_action :set_category, only: [:show, :edit, :update]
     
     def index
         @categories = Category.paginate(page: params[:page], per_page: 25)
@@ -21,15 +22,31 @@ class CategoriesController < ApplicationController
     end
     
     def show
-        @category = Category.find(params[:id])
+        # No longer required
+        # @category = Category.find(params[:id])
         @category_posts = @category.posts.paginate(page: params[:page], per_page: 5)
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        if @category.update(category_params)
+            flash[:success] = "The category was successfully updated"
+            redirect_to categories_path
+        else
+            render 'edit'
+        end
     end
     
     private
         def category_params
             params.require(:category).permit(:name)
         end
-        
+        def set_category
+            @category = Category.find(params[:id])
+        end
         def require_admin_user
             if !logged_in? || !current_user.is_admin
                 flash[:danger] = "You do not have access to category management"
